9553d8a13098c688343ca131a5b4b5fe77e0e0e6
florian dahlitz <f2dahlitz@freenet.de>
2018-07-25 22:21:24 +0200

    feat: added print-statements reveiling the current state of the application

--- src/ListFiles.java
+++ src/ListFiles.java
@@ -15,16 +15,29 @@ public class ListFiles {
     
     public static void main(String[] args) {
         
+        System.out.println("------------------------------");
+        System.out.println("Initialising logger...");
+        System.out.println("------------------------------");
         Logger ocajLogger = Logger.getLogger("OCAJ Logger");
 
         try {
+            System.out.println("Collecting files...");
+            System.out.println("------------------------------");
+
             Files.walk(Paths.get("P:\\src_for_filesystem_applications")).forEach(file -> {
                 if(file.getFileName().toString().endsWith(".txt")) {
                     ocajLogger.log(Level.INFO, "Text doc: " + file.getFileName());
                 }
             });
         } catch (IOException e) {
+            System.out.println("Stopped unexpectatly.");
+            System.out.println("------------------------------");
+
             e.printStackTrace();
         }
+
+        System.out.println("------------------------------");
+        System.out.println("Finished.");
+        System.out.println("------------------------------");
     }
 }
